241ea0c3e6864d7c3ea779562962034d286c0d76
itsmepravin <singh.pravin19@yahoo.com>
2023-03-03 12:35:52 +0545

    Fixed User not logging in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
+
+import { getSession, signOut } from "next-auth/react";
+
+import useCurrentUser from "../hooks/useCurrentUser";
 
 const Home: NextPage = () => {
-  return <h1 className="text-2xl text-orange-400">Netflix Clone</h1>;
+  const { data: user } = useCurrentUser();
+  return (
+    <>
+      <h1 className="text-2xl text-orange-400">Netflix Clone</h1>
+      <p className="text-orange-400">Username: {user?.email}</p>
+      <button
+        onClick={() => signOut()}
+        className="bg-white w-full h-10 rounded-md"
+      >
+        Logout
+      </button>
+    </>
+  );
 };
 
 export default Home;
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
